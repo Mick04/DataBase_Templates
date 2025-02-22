5b3d76eb9fb7ca50d14d6e2371332233aa55c2e4
Mick <mac5y4@talktalk.net>
2025-02-22 18:07:57 +0000

    sorted PubSubClient.h path error

--- EspFirebaseDemo/src/main.cpp
+++ EspFirebaseDemo/src/main.cpp
@@ -12,8 +12,8 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <Firebase_ESP_Client.h>
-#include <pubsubclient.h>
-#include <Adafruit_Sensor.h>
+#include <PubSubClient.h>
+//#include <Adafruit_Sensor.h>
 #include <OneWire.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
